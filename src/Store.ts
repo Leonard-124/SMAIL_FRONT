d1927e2b17bc602b32d38fd035fc79bdb6eb8316
Leonard-124 <leonardoduor91@gmail.com>
2025-11-07 23:18:47 +0300

    Changes

--- src/Store.ts
+++ src/Store.ts
@@ -1,10 +1,10 @@
-import { create } from 'zustand'
+// import { create } from 'zustand'
 
-const useStore = create((set) => ({
-  count: 0,
-  increase: () => set((state) => ({ count: state.count + 1 })),
-  decrease: () => set((state) => ({ count: state.count - 1 })),
-  reset: () => set({ count: 0 }),
-}))
+// const useStore = create((set) => ({
+//   count: 0,
+//   increase: () => set((state) => ({ count: state.count + 1 })),
+//   decrease: () => set((state) => ({ count: state.count - 1 })),
+//   reset: () => set({ count: 0 }),
+// }))
 
-export default useStore
+// export default useStore
