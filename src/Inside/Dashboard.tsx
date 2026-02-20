a9352a81b060ec97c1c338e8255f4737ef615648
Leonard-124 <leonardoduor91@gmail.com>
2026-02-20 14:51:52 +0300

    Frontend Changes

--- src/Inside/Dashboard.tsx
+++ src/Inside/Dashboard.tsx
@@ -1,18 +1,25 @@
 
 
-import Footer from '../Main/Footer';
-import Latest from './Latest';
-import Navbar from './Navbar'
-import Slider from './Slider';
+//import Footer from '../Main/Footer';
+//import Latest from './Latest';
+//import Navbar from './Navbar'
+//import Slider from './Slider';
+
+import Footer from "../components/Home/Footer";
+import Header2 from "../components/Home/Header2";
 
 const Dashboard = () => {
   return (
-    <div>
-      <Navbar/>
-      <Slider/>
-      <Latest/>
-      <Footer/>
+    <>
+    <Header2/>
+      <div>
+      <p className="p-6 m-3.5 text-4xl mt-20">Categories Data Not yet populated</p>
+      {/* <Slider/> */}
+      {/* <Latest/> */}
     </div>
+    <Footer/>
+    </>
+
   )
 }
 
